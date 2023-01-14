095b7f3daabb00e8d7f2c7c8e528251f60fcdbc6
Anuj Kumar Sharma <anuj55149@gmail.com>
2023-01-14 18:13:17 +0530

    added production env

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -9,8 +9,14 @@ import store from '../redux/store';
 import { setLoading, showToast } from "../redux/slices/appConfigSlice";
 import { TOAST_FAILURE } from "../App";
 
+let baseURL = 'http://localhost:4000/';
+console.log('env is ', process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'production') {
+    baseURL = process.env.REACT_APP_SERVER_BASE_URL
+}
+
 export const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_BASE_URL,
+    baseURL,
     withCredentials: true,
 });
 
@@ -46,7 +52,7 @@ axiosClient.interceptors.response.use(async (respone) => {
             .create({
                 withCredentials: true,
             })
-            .get(`${process.env.REACT_APP_SERVER_BASE_URL}/auth/refresh`);
+            .get(`${baseURL}/auth/refresh`);
 
         if (response.data.status === "ok") {
             setItem(KEY_ACCESS_TOKEN, response.data.result.accessToken);
